b013bdb17d3ca904dac3111d5eb6a505df95f092
mrben <mrben1113@gmail.com>
2025-02-20 10:11:52 +0700

    fix prj

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -13,7 +13,7 @@ const Project = () => {
     <section className="c-space my-20" id="work">
         <p className="head-text">My work</p>
         <div className="grid lg:grid-cols-1 grid-cols-1 mt-12 gap-6">
-            <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+            <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 bg-black-600 ">
                 <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={myProjects[0].logoStyle}>
                     <img src={myProjects[0].logo} alt="logo" />
                 </div>
@@ -37,7 +37,7 @@ const Project = () => {
                 </div>
             </div>
             
-            <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+            <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 bg-black-600">
                 <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={myProjects[1].logoStyle}>
                     <img src={myProjects[1].logo} alt="logo" />
                 </div>
